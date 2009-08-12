6a5cfa572e3b8b822b42f3b730f700f86aa69115
Erik M Jacobs <erikmjacobs@gmail.com>
2009-08-12 14:35:40 -0400

    Removed FoodItem model to replace with Food

--- app/models/food_item.rb
+++
@@ -1,2 +0,0 @@
-class FoodItem < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090812183232) do
-
-  create_table "food_items", :force => true do |t|
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20090805205229) do
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
