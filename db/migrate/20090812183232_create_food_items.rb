0c9a2ba1d9d129a4c1c1e1ceed85304d111f33c2
Erik M Jacobs <erikmjacobs@gmail.com>
2009-08-12 14:33:39 -0400

    Added FoodItem model and migrated

---
+++ db/migrate/20090812183232_create_food_items.rb
@@ -0,0 +1,13 @@
+class CreateFoodItems < ActiveRecord::Migration
+  def self.up
+    create_table :food_items do |t|
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :food_items
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090805205229) do
+ActiveRecord::Schema.define(:version => 20090812183232) do
+
+  create_table "food_items", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
