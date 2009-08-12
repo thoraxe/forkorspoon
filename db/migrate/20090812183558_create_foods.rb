eaa4457f924a7377f580f288a724b6d0bd7637b1
Erik M Jacobs <erikmjacobs@gmail.com>
2009-08-12 14:36:34 -0400

    Created Food model

---
+++ db/migrate/20090812183558_create_foods.rb
@@ -0,0 +1,13 @@
+class CreateFoods < ActiveRecord::Migration
+  def self.up
+    create_table :foods do |t|
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :foods
+  end
+end
