63635a76aab0a90db03ef0112c8e2acb93386016
Erik M Jacobs <erikmjacobs@gmail.com>
2009-08-18 16:37:54 -0400

    Timezone tweaks for edit/new user and some other things.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -24,6 +24,26 @@ class FoodsController < ApplicationController
     redirect_to foods_path
   end
 
+  def edit
+    @user = User.find(current_user)
+    @food = Food.find(params[:id])
+    if @user.id != @food.user_id
+      flash[:error] = "Cheeky monkey, you can't edit what's not yours!"
+      redirect_back_or_default(foods_path)
+    end
+  end
+
+  def update
+    @food = Food.find(params[:id])
+    if current_user.id == @food.user_id
+      @food.update_attributes(params[:food])
+      flash[:notice] = "Updated."
+    else
+      flash[:warning] = "Cheeky monkey, you can't edit what's not yours!"
+    end
+    redirect_back_or_default(foods_path)
+  end
+
   def destroy
     @food = Food.find(params[:id])
     @food.destroy
